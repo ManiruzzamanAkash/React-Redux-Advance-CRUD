5646ab13481403cc1e1d28a8a5dfe455a59a06b2
MD. Maniruzzaman Akash <manirujjamanakash@gmail.com>
2020-12-17 02:55:10 +0600

    Sidebar collapse title fixed

--- src/modules/master/layouts/sidebar/Sidebar.jsx
+++ src/modules/master/layouts/sidebar/Sidebar.jsx
@@ -36,12 +36,12 @@ const Sidebar = (props) => {
 
             {/* <!-- Nav Item - Pages Collapse Menu --> */}
             <li className="nav-item">
-                <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="true" aria-controls="collapseTwo">
+                <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#productsCollapse"
+                    aria-expanded="true" aria-controls="productsCollapse">
                     <i class="far fa-list-alt"></i> {" "}
                     <span>Products</span>
                 </a>
-                <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div id="productsCollapse" className="collapse in show" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div className="bg-white py-2 collapse-inner rounded">
                         <h6 className="collapse-header">Manage Products:</h6>
                         <Link className="collapse-item" to="/products">
