e0ead9821dbc4ae34340026f3e65f01bd049da6e
MD. Maniruzzaman Akash <manirujjamanakash@gmail.com>
2020-12-16 22:03:58 +0600

    env file added

--- src/modules/auth/redux/actions/AuthAction.js
+++ src/modules/auth/redux/actions/AuthAction.js
@@ -21,26 +21,31 @@ export const loginSubmitAction = (postData) => async(dispatch) => {
     };
     dispatch({ type: Types.AUTH_LOGIN_CHECK, payload: response });
 
-    await Axios.post(`http://127.0.0.1:8000/api/auth/login`, postData)
-        .then((res) => {
-            const { data, message, status } = res.data;
-            const { access_token, user } = data;
-            response.message = message;
-            response.status = status;
-            if (response.status) {
-                response.access_token = access_token;
-                // Store it to local storage
-                localStorage.setItem('is_logged_in', true);
-                localStorage.setItem('access_token', access_token);
-                localStorage.setItem('userData', JSON.stringify(user));
-                toast.success(response.message);
-            }
-        })
-        .catch((err) => {
-            const message = JSON.parse(err.request.response).message;
-            response.message = message;
-            toast.error(message);
-        });
+    try {
+        await Axios.post(`${process.env.REACT_APP_API_URL}auth/login`, postData)
+            .then((res) => {
+                const { data, message, status } = res.data;
+                const { access_token, user } = data;
+                response.message = message;
+                response.status = status;
+                if (response.status) {
+                    response.access_token = access_token;
+                    // Store it to local storage
+                    localStorage.setItem('is_logged_in', true);
+                    localStorage.setItem('access_token', access_token);
+                    localStorage.setItem('userData', JSON.stringify(user));
+                    toast.success(response.message);
+                }
+            })
+            .catch((err) => {
+                const message = JSON.parse(err.request.response).message;
+                response.message = message;
+                toast.error(message);
+            });
+    } catch (error) {
+        response.message = 'Something Went Wrong !';
+        toast.error(error);
+    }
 
     response.isLoading = false;
     dispatch({ type: Types.AUTH_LOGIN_CHECK, payload: response });
