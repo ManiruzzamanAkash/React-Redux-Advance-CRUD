0be54c0372f29dcab5c6d8a62be133b9b423aaa7
MD. Maniruzzaman Akash <manirujjamanakash@gmail.com>
2020-12-19 07:57:20 +0600

    Toast Helper Added

---
+++ src/modules/master/utils/ToastHelper.js
@@ -0,0 +1,18 @@
+
+import { toast } from 'react-toastify';
+
+export const showToast = (type = 'success', msg, autoClose = 2000, className = "primaryColor") => {
+    if (type === 'success') {
+        toast.success(msg, { 
+            autoClose: autoClose === null ? 2000 : autoClose, 
+            className: className === null ? 'primaryColor' : className, 
+            position: toast.POSITION.TOP_RIGHT 
+        });
+    }else if(type === 'error'){
+        toast.error(msg, { 
+            autoClose: autoClose === null ? 2000 : autoClose, 
+            className: className === null ? 'dangerColor' : className, 
+            position: toast.POSITION.TOP_RIGHT 
+        });
+    }
+}
