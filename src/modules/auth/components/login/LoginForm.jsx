19d648c844a6dffc5cf6167c2f55dc40c314020e
MD. Maniruzzaman Akash <manirujjamanakash@gmail.com>
2020-12-19 16:17:42 +0600

    Login Panel Auto Dummy Data Login Added

--- src/modules/auth/components/login/LoginForm.jsx
+++ src/modules/auth/components/login/LoginForm.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useForm } from "react-hook-form";
 import { withRouter } from 'react-router-dom';
 import { useSelector, useDispatch } from "react-redux";
+import { showToast } from '../../../master/utils/ToastHelper'
 import { getAuthAction, handleChangeLoginInput, loginSubmitAction } from '../../redux/actions/AuthAction';
 
 const LoginForm = withRouter(({ history }) => {
@@ -22,6 +23,12 @@ const LoginForm = withRouter(({ history }) => {
         dispatch(handleChangeLoginInput(name, value));
     };
 
+    const fillAdminData = () => {
+        dispatch(handleChangeLoginInput('email', 'admin@example.com'));
+        dispatch(handleChangeLoginInput('password', '123456'));
+        showToast('success', 'Admin Data Set Successfully, Please Click Login')
+    }
+
     useEffect(() => {
         dispatch(getAuthAction());
         if (typeof loginMessage !== 'undefined' || loginMessage !== null) {
@@ -38,62 +45,71 @@ const LoginForm = withRouter(({ history }) => {
         }
     }, [isLoggedIn, loginMessage, history, authUserData, dispatch]);
     return (
-        <form className="user" onSubmit={handleSubmit(submitHandler)} method="POST">
-            <div className="form-group">
-                <input className="form-control form-control-user"
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="Enter Email Address..."
-                    required=""
-                    aria-required="true"
-                    ref={register({
-                        required: 'Please give your email'
-                    })}
-                    onChange={(e) => handleChangeTextInput('email', e.target.value)}
-                    value={loginData.email}
-                    autoComplete="name"
-                />
-                {
-                    errors.email &&
-                    <div className="text-danger text-sm ml-4">{errors.email.message}</div>
-                }
-            </div>
-            <div className="form-group">
-                <input className="form-control form-control-user"
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder="Enter Password"
-                    required=""
-                    aria-required="true"
-                    ref={register({
-                        required: 'Please give your password'
-                    })}
-                    onChange={(e) => handleChangeTextInput('password', e.target.value)}
-                    value={loginData.password}
-                    autoComplete="name"
-                />
-                {
-                    errors.password &&
-                    <div className="text-danger text-sm ml-4">{errors.password.message}</div>
-                }
-            </div>
+        <>
+            <form className="user" onSubmit={handleSubmit(submitHandler)} method="POST">
+                <div className="form-group">
+                    <input className="form-control form-control-user"
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Enter Email Address..."
+                        required=""
+                        aria-required="true"
+                        ref={register({
+                            required: 'Please give your email'
+                        })}
+                        onChange={(e) => handleChangeTextInput('email', e.target.value)}
+                        value={loginData.email}
+                        autoComplete="name"
+                    />
+                    {
+                        errors.email &&
+                        <div className="text-danger text-sm ml-4">{errors.email.message}</div>
+                    }
+                </div>
+                <div className="form-group">
+                    <input className="form-control form-control-user"
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Enter Password"
+                        required=""
+                        aria-required="true"
+                        ref={register({
+                            required: 'Please give your password'
+                        })}
+                        onChange={(e) => handleChangeTextInput('password', e.target.value)}
+                        value={loginData.password}
+                        autoComplete="name"
+                    />
+                    {
+                        errors.password &&
+                        <div className="text-danger text-sm ml-4">{errors.password.message}</div>
+                    }
+                </div>
 
-            {
-                !isLoading &&
-                <button className="btn btn-primary btn-user btn-block" type="submit">
-                    Login
+                {
+                    !isLoading &&
+                    <button className="btn btn-primary btn-user btn-block" type="submit">
+                        Login <i className="fa fa-sign-out-alt"></i>
+                    </button>
+                }
+                {
+                    isLoading &&
+                    <button className="btn btn-primary btn-user btn-block" type="button" disabled>
+                        Logging In {"  "}
+                        <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                    </button>
+                }
+            </form>
+
+            <div className="mt-3 text-center">
+                <button className="btn btn-info btn-sm" onClick={fillAdminData}>
+                   <i className="fa fa-lock"></i> Fill with Admin Credential
                 </button>
-            }
-            {
-                isLoading &&
-                <button className="btn btn-primary btn-user btn-block" type="button" disabled>
-                    Logging In {"  "}
-                    <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                </button>
-            }
-        </form>
+            </div>
+        </>
+
     );
 })
 
