52e5188500b3741f0fa68b430119d3d3f1e6d449
MD. Maniruzzaman Akash <manirujjamanakash@gmail.com>
2020-12-19 16:40:31 +0600

    Dashboard Product Count and Link added

--- src/modules/master/views/DashboardContainer.jsx
+++ src/modules/master/views/DashboardContainer.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from '../layouts/Layout';
+import { useSelector, useDispatch } from "react-redux";
+import { getProductsAction } from '../../products/redux/actions/ProductAction';
+import { Link } from 'react-router-dom';
 
 const DashboardContainer = () => {
+    const dispatch = useDispatch();
+    const products = useSelector((state) => state.product.productsPaginatedData);
+
+    useEffect(() => {
+        dispatch(getProductsAction(1));
+    }, [dispatch]);
+
     return (
         <Layout sidebar={true} dashboard={true}>
             <div className="d-sm-flex align-items-center justify-content-between mb-4">
@@ -17,11 +27,16 @@ const DashboardContainer = () => {
                                         Total Products
                                     </div>
                                     <div className="h5 mb-0 font-weight-bold text-gray-800">
-                                        10 Pcs
+                                        {products != null && products.total != null ? products.total : 0} Pcs
                                     </div>
                                 </div>
                                 <div className="col-auto">
                                     <i className="fas fa-calculator fa-3x text-gray-300"></i>
+                                </div>
+                                <div className="col-12 mt-3">
+                                    <Link to="/products" className="btn btn-sm btn-outline-info">
+                                        Manage Products <i className="fa fa-cog"></i>
+                                    </Link>
                                 </div>
                             </div>
                         </div>
@@ -33,14 +48,16 @@ const DashboardContainer = () => {
                             <div className="row no-gutters align-items-center">
                                 <div className="col mr-2">
                                     <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                        Total Value
+                                        Create New
                                     </div>
-                                    <div className="h5 mb-0 font-weight-bold text-gray-800">
-                                        10000 ৳
+                                    <div className="col-12 mt-3">
+                                        <Link to="/products/create" className="btn btn-sm btn-outline-info">
+                                            Add Product <i className="fa fa-plus-circle"></i>
+                                        </Link>
                                     </div>
                                 </div>
                                 <div className="col-auto">
-                                    <i className="fas fa-money-check-alt fa-3x text-gray-300" ></i>
+                                    <i className="fas fa-shopping-cart fa-3x text-gray-300" ></i>
                                 </div>
                             </div>
                         </div>
