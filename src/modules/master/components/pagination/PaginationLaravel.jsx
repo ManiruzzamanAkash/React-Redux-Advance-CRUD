e69eceaf1f7cb78f3f77b90d8dd28c1463cf884a
MD. Maniruzzaman Akash <manirujjamanakash@gmail.com>
2020-12-19 09:20:05 +0600

    Custom Pagination Added

--- src/modules/master/components/pagination/PaginationLaravel.jsx
+++ src/modules/master/components/pagination/PaginationLaravel.jsx
@@ -5,11 +5,21 @@ const PaginationLaravel = (props) => {
     const { data, changePage } = props;
     return ( 
         <>
-        <div class="d-flex">
-            <div class="mx-auto">
-                <Pagination changePage={changePage} data={data}/>
+        {
+            data !== null &&
+            <div className="d-flex">
+                <div className="mx-auto">
+                    <div className=''>
+                        <div>
+                            Products - {data.from} to {data.from + data.per_page}  | Total {data.per_page} Out of {data.total}
+                        </div>
+                        <div>
+                            <Pagination changePage={changePage} data={data}/>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        }
         </>
      );
 }
